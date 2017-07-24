695fd3accd07d288c3dd788482a928a3ced318ca
Amanda <ajhwhiz@gmail.com>
2017-07-24 16:19:03 -0700

    changes to doc component styles

--- reactApp/components/DocComponentStyles.js
+++ reactApp/components/DocComponentStyles.js
@@ -41,11 +41,11 @@ const BlockStyleControls = (props) => {
     <div className="RichEditor-controls">
     {BLOCK_TYPES.map((type) =>
       <StyleButton
-      key={type.label}
-      active={type.style === blockType}
-      label={type.label}
-      onToggle={props.onToggle}
-      style={type.style}
+        key={type.label}
+        active={type.style === blockType}
+        label={type.label}
+        onToggle={props.onToggle}
+        style={type.style}
       />
     )}
     </div>
@@ -63,11 +63,11 @@ const InlineStyleControls = (props) => {
     <div className="RichEditor-controls">
     {INLINE_STYLES.map(type =>
       <StyleButton
-      key={type.label}
-      active={currentStyle.has(type.style)}
-      label={type.label}
-      onToggle={props.onToggle}
-      style={type.style}
+        key={type.label}
+        active={currentStyle.has(type.style)}
+        label={type.label}
+        onToggle={props.onToggle}
+        style={type.style}
       />
     )}
     </div>
