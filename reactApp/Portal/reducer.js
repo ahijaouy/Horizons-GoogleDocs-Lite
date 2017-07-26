50c306b8d493b43d76477de90f843fb9fc0f4e93
Oberon <ahijaouy@gatech.edu>
2017-07-25 21:49:43 -0700

    added skeleton for Document and Portal agreements

---
+++ reactApp/Portal/reducer.js
@@ -0,0 +1,12 @@
+import * as types from './types';
+const initialState = {
+};
+
+function portalReducer(state = initialState, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
+
+export default portalReducer;
